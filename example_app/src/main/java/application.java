cb2fcfcfedd0cba27a74f641ecbbb29145d24b12
Kate Travers <ktravers@udel.edu>
2018-03-15 20:05:52 -0400

    second commit

---
+++ example_app/src/main/java/application.java
@@ -0,0 +1,13 @@
+
+public class application {
+	
+	public static void main(String [] args) {
+		application app = new application();
+		app.print_stuff();
+	}
+	
+	public void print_stuff() {
+		System.out.println("hello world");
+	}
+
+}
